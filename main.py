a9186ac0f0b61b4ce1ca029c1714e5dd5f3867a6
Plathe <git@pm-gitlab.intranet.inp-greifswald.de>
2022-01-18 13:01:57 +0100

    Committing initial PyCharm Dir

---
+++ main.py
@@ -0,0 +1,10 @@
+#----------------------------------------
+# pyJSON Converter GUI
+# author: N. Plathe
+#----------------------------------------
+# Libraries
+#----------------------------------------
+
+from PyQt5 import QtWidgets
+import sys
+
