54217fa545bb8ce77ad086e48aa11e388e177ba9
Plathe <git@pm-gitlab.intranet.inp-greifswald.de>
2022-01-24 12:17:13 +0100

    Added in wrapper for json.JSONDecoder and a function, that generates a python nested dict from a given schema ready for editing and to be converted to a proper JSON later.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # ----------------------------------------
-# pyJSON Converter GUI
+# pyJSON Converter + GUI
 # author: N. Plathe
 # ----------------------------------------
-# Music recommendation:
+# Music recommendation (albums):
 # Feuerschwanz - Memento Mori
+# Bullet for my Valentine - Bullet for my Valentine
 # ----------------------------------------
 # Libraries
 # ----------------------------------------
@@ -39,8 +40,44 @@ def validator_function(json_path, json_schema_path):
     return 0
 
 
+def decode_function(json_path):
+    try:
+        loaded_json = open(json_path)
+        result = json.load(loaded_json, cls=json.JSONDecoder)
+    except json.JSONDecodeError as err:
+        print("[decode_function/ERROR]: JSON could not be parsed into Python representation!")
+        return 2
+    except OSError:
+        print("[decode_function/ERROR]: JSON is not accessible anymore!")
+        return -999
+    return result
+
+
+def schema_to_py_gen(decoded_schema):
+    return_dict = {}
+    for element in decoded_schema["properties"]:
+        #print(element)
+        #print(decoded_schema["properties"][element])
+        #print("-------------------------------")
+        match decoded_schema["properties"][element]["type"]:
+            case "string":
+                return_dict[element] = ""
+            case "array":
+                return_dict[element] = []
+            case "number":
+                return_dict[element] = float("NaN")
+            case "object":
+                return_dict[element] = schema_to_py_gen(decoded_schema["properties"][element])
+    return return_dict
+
+
 # ----------------------------------------
 # Execution
 # ----------------------------------------
 
-print(validator_function("C:\\Users\\plathe\\Desktop\\Franke_Orga\\wrong.json", "C:\\Users\\plathe\\Desktop\\Franke_Orga\\UBER.JSON"))
+print(validator_function('C:\\Users\\plathe\\Desktop\\Franke_Orga\\wrong.json',
+                         'C:\\Users\\plathe\\Desktop\\Franke_Orga\\UBER.JSON'))
+
+frame = decode_function('C:\\Users\\plathe\\Desktop\\Franke_Orga\\UBER.JSON')
+pre_json = schema_to_py_gen(frame)
+print("Ping")
